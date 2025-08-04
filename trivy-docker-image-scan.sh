bf39b4e86570115fda8213cd12508fbca9e9191e
Manashree-12 <107397961+Manashree-12@users.noreply.github.com>
2025-08-04 10:58:41 +0530

    Update trivy-docker-image-scan.sh

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+
 dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
-echo $dockerImageName
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.58.1 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
+echo "$dockerImageName"
+
+docker run --rm -v "$WORKSPACE:/root/.cache/" aquasec/trivy:0.58.1 -q image --exit-code 1 --severity CRITICAL --light "$dockerImageName"
+
 # Trivy scan result processing
 exit_code=$?
 echo "Exit Code : $exit_code"
- Check scan results
-if [[ "${exit_code}" == 1 ]]; then
+
+# Check scan results
+if [[ "$exit_code" == 1 ]]; then
     echo "Image scanning failed. Vulnerabilities found, not failing build"
     echo "---------------------------------------------------------------"
-#   echo "Please fix the vulnerabilities"
- #  exit 0;
+    # echo "Please fix the vulnerabilities"
+    # exit 0
 else
-#   echo "Image scanning passed. No CRITICAL vulnerabilities found"
+    echo "Image scanning passed. No CRITICAL vulnerabilities found"
 fi
